ec5fabf4d2bb0e6359a7a164345368e5c45f3de9
saeedkokash <saeedkokash@gmail.com>
2022-09-12 14:49:45 +0300

    added server to test on heroku

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+require('dotenv').config();
+const server = require('./server');
+const { db } = require('./models/index')
+
+db.sync().then(() => {
+    server.start(process.env.PORT || 4000);
+}).catch(console.error)
---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const postRouter = require('./routes/post.route');
+const errorHandler = require('./error-handlers/500');
+const notFoundHandler = require('./error-handlers/404');
+
+app.use(cors());
+app.use(express.json());
+app.use(postRouter);
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Home Page',
+        code: 200
+    })
+})
+
+app.use(errorHandler);
+app.use(notFoundHandler);
+
+function start(port) {
+    app.listen(port, () => console.log(`Server is starting on port ${port}`))
+}
+
+module.exports = {
+    start
+};
