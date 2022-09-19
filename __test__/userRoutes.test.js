e58f6b5af2bafffeba7697649ad1bb34848a0e87
saeedkokash <saeedkokash@gmail.com>
2022-09-19 16:14:20 +0300

    added user model, controller, middleware, tests for user

---
+++ __test__/userRoutes.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const supertest = require('supertest');
+const server = require('../server');
+const request = supertest( server.app );
+
+describe('Testing user Routes', () => {
+
+    it('testing getting all users from /users', async () => {
+        const res = await request.get( '/users' );
+        expect( res.status ).toEqual( 200 );
+    });
+
+    // can only be tested once
+    // test('testing sign up a new user ', async () => {
+    //     const res = await request.post( '/signup' ).send({
+    //         userName: 'Testing Account',
+    //         email: 'testing@testing.com',
+    //         password: 'test'
+    //     });
+    //     expect(res.status).toEqual(201);
+    // })
+
+    test('testing sign up with an existing username', async () => {
+        const res = await request.post( '/signup' ).send({
+            userName: 'Testing Account',
+            email: 'nottesting@testing.com',
+            password: 'test'
+        });
+        expect(res.status).toEqual(409);
+        expect(res.text).toEqual('Username already exists')
+    })
+
+    test('testing sign up with an existing email', async () => {
+        const res = await request.post( '/signup' ).send({
+            userName: 'not Testing Account',
+            email: 'testing@testing.com',
+            password: 'test'
+        });
+        expect(res.status).toEqual(409);
+        expect(res.text).toEqual('Email already exists')
+    })
+    
+});
---
+++ middlewares/userAuth.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const User = require('../models/index').userModel;
+
+const userAuth = async (req, res, next) => {
+    try {
+
+        // we need to search for username in the Database
+        const userName =  await User.findOne({
+            where: {userName: req.body.userName}
+        })
+
+        if (userName) {
+            return res.status(409).send('Username already exists')
+        }
+    
+        // we need to search for the email in the Database
+        const email =  await User.findOne({
+            where: {email: req.body.email}
+        })
+
+        if (email) {
+            return res.status(409).send('Email already exists')
+        }
+    
+        next();
+
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+module.exports = { userAuth };
